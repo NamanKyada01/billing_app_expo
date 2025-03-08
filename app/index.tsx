bea8f3555e66eab04ecc8419a937a581b5bb3e05
callmenk36 <callmenk36@gmail.com>
2025-03-08 23:11:43 +0530

    prettier || tailwind || colours setup completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const aa = 'he';
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-center text-5xl text-blue-500 font-bold">
+                Hiyo.
+            </Text>
+        </View>
+    );
 }
