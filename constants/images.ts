4784c44214b914d1a2a99ed5e1fafcb166f86b37
callmenk36 <callmenk36@gmail.com>
2025-03-09 12:42:32 +0530

    appwrite added || tab added || colors and components created

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,21 @@
-import bg from "@/assets/images/bg.png";
-import highlight from "@/assets/images/highlight.png";
-import rankingGradient from "@/assets/images/rankingGradient.png";
+import onboarding from '@/assets/images/onboarding.png'
+import avatar from '@/assets/images/avatar.png'
+import newYork from '@/assets/images/new-york.png'
+import japan from '@/assets/images/japan.png'
+import cardGradient from '@/assets/images/card-gradient.png'
+import barChart from '@/assets/images/bar-chart.png'
+import whiteGradient from '@/assets/images/white-gradient.png'
+import map from '@/assets/images/map.png'
+import noResult from '@/assets/images/no-result.png'
 
-export const images = {
-  bg,
-  highlight,
-  rankingGradient,
-};
+export default {
+    onboarding,
+    avatar,
+    newYork,
+    japan,
+    cardGradient,
+    barChart,
+    whiteGradient,
+    map,
+    noResult,
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     // NOTE: Update this to include the paths to all of your component files.
-    content: ['./app/**/*.{js,jsx,ts,tsx}'],
+    content: [
+        './app/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+    ],
     presets: [require('nativewind/preset')],
     theme: {
         extend: {
+            fontFamily: {
+                rubik: ['Rubik-Regular', 'sans-serif'],
+                'rubik-bold': ['Rubik-Bold', 'sans-serif'],
+                'rubik-extrabold': ['Rubik-ExtraBold', 'sans-serif'],
+                'rubik-medium': ['Rubik-Medium', 'sans-serif'],
+                'rubik-semibold': ['Rubik-Semibold', 'sans-serif'],
+                'rubik-light': ['Rubik-Light', 'sans-serif'],
+            },
             colors: {
-                primary: '#030014',
-                secondary: '#151312',
+                primary: {
+                    100: '#0061FF0A',
+                    200: '#0061FF1A',
+                    300: '#0061FF',
+                },
+                accent: {
+                    100: '#FBFBFD',
+                },
+                black: {
+                    DEFAULT: '#000000',
+                    100: '#8C8E98',
+                    200: '#666876',
+                    300: '#191d31',
+                },
+                danger: '#F75555',
+
                 light: {
                     100: '#D6C6FF',
                     200: '#A8B5DB',
@@ -17,7 +42,6 @@ module.exports = {
                     100: '#221f3d',
                     200: '#0f0d23',
                 },
-                accent: '#AB8BFF',
             },
         },
     },
